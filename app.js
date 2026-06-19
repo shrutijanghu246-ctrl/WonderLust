fc7acd3a8f978e8a9a0ae41974c85df5eb198b1b
shrutijanghu246-ctrl <shrutijanghu246@gmail.com>
2026-06-19 16:46:45 +0530

    removed crypto

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  crypto: { secret: process.env.SECRET },
   touchAfter: 24 * 3600,
 });
 
